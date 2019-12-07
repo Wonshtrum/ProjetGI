8197ab2bfa015876e6d713d8441c54cc1e8dbd18
bouillonva <bouillonva@eisti.eu>
2019-12-07 16:40:54 +0100

    starter

---
+++ app/src/main/java/com/example/applimedecin/FormulaireActivity.kt
@@ -0,0 +1,12 @@
+package com.example.applimedecin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormulaireActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulaire)
+    }
+}
