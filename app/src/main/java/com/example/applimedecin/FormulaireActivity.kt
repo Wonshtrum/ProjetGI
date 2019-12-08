63b127036312fe83f46d06ab657745bff0865173
OrdilHub <tikollen6@gmail.com>
2019-12-08 15:29:41 +0100

    Ajout première page du formulaire "Autre"

--- app/src/main/java/com/example/applimedecin/FormulaireActivity.kt
+++ app/src/main/java/com/example/applimedecin/FormulaireActivity.kt
@@ -3,10 +3,13 @@ package com.example.applimedecin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class FormulaireActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formulaire)
+
     }
+
 }
