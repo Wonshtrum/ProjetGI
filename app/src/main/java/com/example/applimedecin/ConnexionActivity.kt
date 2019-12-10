b092de9bd9f1d516acf33b1a866399dc834d790d
Ornitro <hoderomain@eisti.eu>
2019-12-10 18:21:59 +0100

    Connexion page

--- app/src/main/java/com/example/applimedecin/ConnexionActivity.kt
+++ app/src/main/java/com/example/applimedecin/ConnexionActivity.kt
@@ -1,12 +1,21 @@
 package com.example.applimedecin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_connexion.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ConnexionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connexion)
+        buttonCreerUnCompte.setOnClickListener {
+            startActivity(Intent(this@ConnexionActivity, FormulaireActivity::class.java))
+        }
+        buttonRetour.setOnClickListener{
+            startActivity(Intent(this@ConnexionActivity, MainActivity::class.java))
+        }
     }
 }
