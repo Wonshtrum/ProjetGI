d10e159d2bf8532caf2f39808d07a6a8fe4fa32b
Ornitro <hoderomain@eisti.eu>
2019-12-10 16:43:01 +0100

    Connexion page

---
+++ app/src/main/java/com/example/applimedecin/ConnexionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.applimedecin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConnexionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connexion)
+    }
+}
