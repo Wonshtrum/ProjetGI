a57e4c812ea7ed2d54f78fe94d75795c6c9cc464
OrdilHub <tikollen6@gmail.com>
2019-12-10 18:06:06 +0100

    Ajout de la fonctionnalité permettant d'aller d'une page de fourmualire autre à la suivante et autres

--- app/src/main/java/com/example/applimedecin/FormulaireAutreDActivity.kt
+++ app/src/main/java/com/example/applimedecin/FormulaireAutreDActivity.kt
@@ -2,6 +2,11 @@ package com.example.applimedecin
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_formulaire.*
 
@@ -10,5 +15,38 @@ class FormulaireAutreDActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formulaire_autre_d)
+        val editChamps1 = findViewById<EditText>(R.id.editTextKMMax);
+        val editChamps2 = findViewById<EditText>(R.id.editTextRayonVisite);
+        val editSwitch = findViewById<Switch>(R.id.switchVisite);
+        val boutonValider = findViewById<Button>(R.id.boutonSuivantD);
+
+        editSwitch.setOnCheckedChangeListener{ buttonView, isChecked ->
+            println(isChecked)
+            if (isChecked) {
+                editChamps2.setEnabled(true)
+            }else{
+                editChamps2.setEnabled(false)
+            }
+        }
+
+        editChamps1.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                if (!editChamps1.getText().toString().trim().isEmpty()) {
+                    boutonValider.setEnabled(true);
+                }
+            }
+        })
     }
 }
